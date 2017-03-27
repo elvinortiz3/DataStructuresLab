87f6575b309736e38de09c5370ba373b38c5a064
4035b20 <4035b20@guanabana.ece.uprm.edu>
2017-03-27 17:50:06 -0400

    Lab 9 commit - PLR

---
+++ Lab 9 Priority Queue - PLR/src/testerClasses/PriorityQueueTester3.java
@@ -0,0 +1,20 @@
+package testerClasses;
+
+import priorityQueue.UnsortedListPriorityQueue;
+import priorityQueueInterfaces.PriorityQueue;
+
+public class PriorityQueueTester3 {
+	
+	public static void main(String[] args) {
+		
+		String[] arr = {"barrio", "pepe", "julia", "maria", "oliva", "meme", "parada", "baile", "enjendro", 
+				"vagabundo", "nota", "tienda", "zapato", "caballo", "cafe", "diodo", "multiplica"}; 
+
+		PriorityQueue<String, String> pq = new UnsortedListPriorityQueue<String, String>(new StringComparator1());
+
+		TesterUtils.displayArray("Content of the array before ", arr); 
+		TesterUtils.what(arr, pq); 
+		TesterUtils.displayArray("Content of the array after ", arr); 
+		
+	}
+}
